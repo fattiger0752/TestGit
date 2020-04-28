0c19bca5d5fbbe80b763213a1bcf6fa6ed384a4a
zjj <zjj@qq.com>
2020-04-28 22:10:53 +0800

    commit test

--- src/main/java/com/zjj/git/Add.java
+++ src/main/java/com/zjj/git/Add.java
@@ -4,5 +4,6 @@ public class Add {
 	public static void main(String[] args) {
 		System.out.println("hot fix");
 		System.out.println("Idea add by zjj");
+		System.out.println("Idea add by zjj1");
 	}
 }
